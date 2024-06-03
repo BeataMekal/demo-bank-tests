51cec7633c78377428da54d89cf9eab9ae5367a4
Beata Lapek <beatakuzlecka@o2.pl>
2024-06-03 22:33:42 +0200

    added prettier

--- README.md
+++ README.md
@@ -1,34 +1,40 @@
 # Test Automation training form jaktestowac.pl
+
 ## Links
+
 - course https://jaktestowac.pl/course/playwright-wprowadzenie/
 - test site
-https://demo-bank.vercel.app/  
-If link broken check first lesson for update:
-https://jaktestowac.pl/lesson/pw1s01l01/
+  https://demo-bank.vercel.app/  
+  If link broken check first lesson for update:
+  https://jaktestowac.pl/lesson/pw1s01l01/
 - code repository https://github.com/jaktestowac/playwright_automatyzacja_wprowadzenie
 
 ## Commands
-- check `NodeJS` version    
-`node -v`
+
+- check `NodeJS` version  
+  `node -v`
 - new project with Playwright:  
-`npm init playwright@latest`
+  `npm init playwright@latest`
 - record tests for given site  
-`npx playwright codegen https://demo-bank.vercel.app/`
+  `npx playwright codegen https://demo-bank.vercel.app/`
 - run tests without browser GUI:  
-`npx playwright test`
+  `npx playwright test`
 - run test with browser GUI:  
-`npx playwright test --headed`
+  `npx playwright test --headed`
 - viewing report  
-`npx playwright show-report`
+  `npx playwright show-report`
+
 ## Playwright Config modifications
+
 - config file `playwright.config.ts`
 - disabling browsers, i.e. Firefox:
-    ```json
-    // {
-    //   name: 'firefox',
-    //   use: {
-    //     ...devices['Desktop Firefox'],
-    //   },
-    // },
-    ```
+  ```json
+  // {
+  //   name: 'firefox',
+  //   use: {
+  //     ...devices['Desktop Firefox'],
+  //   },
+  // },
+  ```
+
 ## Playwhright snippets
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,37 +1,43 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test.describe("User login to Demobank", () => { //grupa testów
-  test("successful login with correct credentials", async ({ page }) => {
+test.describe('User login to Demobank', () => {
+  //grupa testów
+  test('successful login with correct credentials', async ({ page }) => {
     // Arrange
     const url = 'https://demo-bank.vercel.app/';
-    const userId = "testerLO";
-    const userPassword = "12345678";
-    const expectedUserName = "Jan Demobankowy";
+    const userId = 'testerLO';
+    const userPassword = '12345678';
+    const expectedUserName = 'Jan Demobankowy';
 
     // Act
     await page.goto(url);
-    await page.getByTestId("login-input").fill(userId);
-    await page.getByTestId("password-input").fill(userPassword);
-    await page.getByTestId("login-button").click();
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
+    await page.getByTestId('login-button').click();
 
     //Assert
-    await expect(page.getByTestId("user-name")).toHaveText(expectedUserName);
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
   });
 
-  test("unsuccessful login with too short username", async ({ page }) => {
+  test('unsuccessful login with too short username', async ({ page }) => {
     await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId("login-input").fill("tester");
-    await page.getByTestId("password-input").click();
+    await page.getByTestId('login-input').fill('tester');
+    await page.getByTestId('password-input').click();
 
-    await expect(page.getByTestId("error-login-id")).toHaveText("identyfikator ma min. 8 znaków");
+    await expect(page.getByTestId('error-login-id')).toHaveText(
+      'identyfikator ma min. 8 znaków',
+    );
   });
 
-  test("unsuccessful login with too short password", async ({ page }) => { // uruchomi się tylko ten test
+  test('unsuccessful login with too short password', async ({ page }) => {
+    // uruchomi się tylko ten test
     await page.goto('https://demo-bank.vercel.app/');
     await page.getByTestId('login-input').fill('testerLO');
     await page.getByTestId('password-input').fill('1234');
     await page.getByTestId('password-input').blur(); // wyjście z focusa
 
-    await expect(page.getByTestId("error-login-password")).toHaveText("hasło ma min. 8 znaków");
+    await expect(page.getByTestId('error-login-password')).toHaveText(
+      'hasło ma min. 8 znaków',
+    );
   });
 });
