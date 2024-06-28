2daf66109cbfca5237b2ce7dc6f63a94315deb29
Beata Lapek <beatakuzlecka@o2.pl>
2024-06-28 17:32:48 +0200

    login data

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { loginData } from '../test-data/login.data';
 
 test.describe('User login to Demobank', () => {
   //--group of tests
@@ -8,8 +9,8 @@ test.describe('User login to Demobank', () => {
 
   test('successful login with correct credentials', async ({ page }) => {
     // Arrange
-    const userId = 'testerLO';
-    const userPassword = '12345678';
+    const userId = loginData.userId;
+    const userPassword = loginData.password;
     const expectedUserName = 'Jan Demobankowy';
 
     // Act
@@ -38,7 +39,7 @@ test.describe('User login to Demobank', () => {
 
   test('unsuccessful login with too short password', async ({ page }) => {
     // Arrange
-    const userId = 'testerLO';
+    const userId = loginData.userId;
     const incorrectUserPassword = '1234';
     const expectedErrorMessage = 'hasło ma min. 8 znaków';
 
